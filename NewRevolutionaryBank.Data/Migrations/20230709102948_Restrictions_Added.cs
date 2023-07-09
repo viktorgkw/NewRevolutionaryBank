d0c7cd3a92a2eba9fea5252069fec9bad3d8246a
viktorgkw <ciganskigeigerwiksa26g@gmail.com>
2023-07-09 13:31:42 +0300

    Added migration with new property restrictions.

---
+++ NewRevolutionaryBank.Data/Migrations/20230709102948_Restrictions_Added.cs
@@ -0,0 +1,60 @@
+﻿namespace NewRevolutionaryBank.Data.Migrations;
+
+using Microsoft.EntityFrameworkCore.Migrations;
+
+public partial class Restrictions_Added : Migration
+{
+    protected override void Up(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "LastName",
+            table: "AspNetUsers",
+            type: "nvarchar(46)",
+            maxLength: 46,
+            nullable: true,
+            comment: "Фамилия",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true,
+            oldComment: "Фамилия");
+
+        migrationBuilder.AlterColumn<string>(
+            name: "FirstName",
+            table: "AspNetUsers",
+            type: "nvarchar(46)",
+            maxLength: 46,
+            nullable: true,
+            comment: "Собствено име",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(max)",
+            oldNullable: true,
+            oldComment: "Собствено име");
+    }
+
+    protected override void Down(MigrationBuilder migrationBuilder)
+    {
+        migrationBuilder.AlterColumn<string>(
+            name: "LastName",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true,
+            comment: "Фамилия",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(46)",
+            oldMaxLength: 46,
+            oldNullable: true,
+            oldComment: "Фамилия");
+
+        migrationBuilder.AlterColumn<string>(
+            name: "FirstName",
+            table: "AspNetUsers",
+            type: "nvarchar(max)",
+            nullable: true,
+            comment: "Собствено име",
+            oldClrType: typeof(string),
+            oldType: "nvarchar(46)",
+            oldMaxLength: 46,
+            oldNullable: true,
+            oldComment: "Собствено име");
+    }
+}
